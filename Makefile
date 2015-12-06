b0354392fb50b31bbdc6e3d05dd6e2e4099d4e24
hoenirvili <hoenirvili@gmail.com>
2015-12-06 23:04:27 +0200

    timeout added

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 build:
 	go build
+	./Pix --path text/mecha.txt --env frame/postTaggerFrame.xml --save req0.xml -0
+	./Pix --path text/mecha.txt --env frame/npChunkerFrame.xml --save req1.xml -1
+	./Pix --path text/mecha.txt --env frame/fdgParserFrame.xml --save req2.xml -2
+	./Pix --path text/mecha.txt --env frame/nameEntityRecogn.xml --save req3.xml -3
+	./Pix --path text/mecha.txt --env frame/anaphoraResolution.xml --save req4.xml -4
+	./Pix --path text/mecha.txt --env frame/clauseSplit.xml --save req5.xml -5
 	./Pix --path text/mecha.txt --env frame/discourseParser.xml --save req6.xml -6
 clean:
 	rm -rf newRequest.xml
--- lib/soap.go
+++ lib/soap.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func sendEnvelopeRequest(env interface{}, url string) []byte {
@@ -21,16 +22,20 @@ func sendEnvelopeRequest(env interface{}, url string) []byte {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(xml))
 	req.Header.Set("Content-Type", "text/xml;charset=UTF-8")
 
-	// Create new client that will do the request
-	client := &http.Client{}
+	timeout := time.Duration(10 * time.Second)
+	// Create new client that will do the request and after 10 seconds of waiting just timeout
+	client := &http.Client{
+		Timeout: timeout,
+	}
 
-	fmt.Fprintf(os.Stdout, "%s ===> %s\n", blue("Sending request"), green(url))
+	fmt.Fprintf(os.Stdout, "%s ===> %s\n", blue("REQUEST"), green(url))
 
 	// Do the request
 	response, err := client.Do(req)
 
 	if err != nil {
-		ErrNow("Client can't do the specific request")
+		fmt.Fprintf(os.Stderr, "%s ===> %s\n", yellow("TIMEOUT"), green(url))
+		os.Exit(0)
 	}
 
 	defer response.Body.Close()
@@ -39,6 +44,7 @@ func sendEnvelopeRequest(env interface{}, url string) []byte {
 	fmt.Println(green("Reponse Headers"), blue(response.Header))
 
 	body, err := ioutil.ReadAll(response.Body)
+
 	if err != nil {
 		ErrNow("Can't read body response ")
 	}
